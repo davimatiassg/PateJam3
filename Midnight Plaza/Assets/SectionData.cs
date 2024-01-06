53f36e0d29fa03df6a637c2d3078fe96649c2a64
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-01-06 15:11:37 -0300

    CityBuilder

---
+++ Midnight Plaza/Assets/SectionData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionData : MonoBehaviour
+{
+    public List<GameObject> sections = new List<GameObject>();
+
+    void Start() {}
+    void Update() {}
+}
