6a32b834d9aefec2884235a33f79a8b549e2c377
davimatiassg <davimatiassg@gmail.com>
2024-01-07 18:34:37 -0300

    Animation Updates

--- Midnight Plaza/Assets/Scripts/UI/Scorer.cs
+++ Midnight Plaza/Assets/Scripts/UI/Scorer.cs
@@ -6,16 +6,45 @@ using TMPro;
 [RequireComponent(typeof(TextMeshProUGUI))]
 public class Scorer : MonoBehaviour
 {
-    private int score;
+    public static Scorer Instance;
+    public int score;
+
+    public static int Score
+    {
+        get
+        {
+            if(Instance == null) { return 0; }
+            return Instance.score;
+        }
+        set
+        {
+            Debug.Log(value);
+            if(Instance == null) { return; }
+            Instance.score = value;
+            Instance.UpdateScoreText();
+        }        
+    }
     private TextMeshProUGUI textMesh;
     private void OnEnable() {
+        if(Scorer.Instance == null){ Scorer.Instance = this; }
+        else if(Scorer.Instance != this) {Destroy(this.gameObject); return; }
         textMesh = GetComponent<TextMeshProUGUI>();
-        GameDataManager.Instance.onGainScore += UpdateScore;
+        GameDataManager.Instance.onGainScore += AddScore;
     }
 
-    public void UpdateScore(IValuable value)
+    public void Start() {
+        score = 100;
+        UpdateScoreText();
+    }
+
+    public void AddScore(IValuable value)
     {
         score += value.ScoreValue;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText() {
         textMesh.text = string.Format("{0}", score);
     }
+
 }
