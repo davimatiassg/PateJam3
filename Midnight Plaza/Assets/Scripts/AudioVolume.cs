1ff9f0fb37100586c9f6420ddb235e4587375820
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-01-07 18:32:14 -0300

    added [RequiresComponent(typeof(AudioSource))]

--- Midnight Plaza/Assets/Scripts/AudioVolume.cs
+++ Midnight Plaza/Assets/Scripts/AudioVolume.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioVolume : MonoBehaviour
 {
     private AudioSource audioSource;
